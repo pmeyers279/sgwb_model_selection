53272180b842833ac5d94826e4f6d0f4e118143f
Patrick Meyers <pmeyers279@gmail.com>
2017-02-08 10:22:03 -0600

    update test

--- sbms/tests/models_test.py
+++ sbms/tests/models_test.py
@@ -2,14 +2,15 @@ import unittest
 from ..io_sbms import read_ini
 from ..models import *
 import numpy.testing as npt
+import numpy as np
 
 class TestModel(unittest.TestCase):
     def test_power_law(self):
         params = read_ini('sbms/tests/data/test_ini.ini')
-        omgw_f,f = power_law.omega_gw_spectrum(1e-9, alpha=0)
+        omgw_f,f = power_law.omega_gw_spectrum(-9, alpha=0)
         npt.assert_array_almost_equal(omgw_f, 1e-9*np.ones(omgw_f.size))
     def test_broken_power_law(self):
-        omgw_f,f = broken_power_law.omega_gw_spectrum(1e-9, f_break=50,
+        omgw_f,f = broken_power_law.omega_gw_spectrum(-9, f_break=50,
                 alpha1=0, alpha2=0)
         npt.assert_array_almost_equal(omgw_f, 1e-9*np.ones(omgw_f.size))
 
