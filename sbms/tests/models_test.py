33046a92673465deee4ad630dc0550a8113a2bb5
Patrick Meyers <pmeyers279@gmail.com>
2017-02-08 10:19:23 -0600

    add broken power law tests

--- sbms/tests/models_test.py
+++ sbms/tests/models_test.py
@@ -1,12 +1,17 @@
 import unittest
 from ..io_sbms import read_ini
 from ..models import *
+import numpy.testing as npt
 
 class TestModel(unittest.TestCase):
     def test_power_law(self):
         params = read_ini('sbms/tests/data/test_ini.ini')
-        print params
         omgw_f,f = power_law.omega_gw_spectrum(1e-9, alpha=0)
-#        omgw_f2,f = omgwf(params)
+        npt.assert_array_almost_equal(omgw_f, 1e-9*np.ones(omgw_f.size))
+    def test_broken_power_law(self):
+        omgw_f,f = broken_power_law.omega_gw_spectrum(1e-9, f_break=50,
+                alpha1=0, alpha2=0)
+        npt.assert_array_almost_equal(omgw_f, 1e-9*np.ones(omgw_f.size))
+
 if __name__=="__main__":
     unittest.main()
